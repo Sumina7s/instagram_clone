febeebb9f577ead277da99ab1d3cc8708f82d4d2
Sumina7s <asumina54@gmail.com>
2023-05-22 12:56:37 +0545

    changed

---
+++ insta_clone/insta_clone/lib/responsive/responsive_layout_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class ResponssiveLayout extends StatelessWidget {
+  const ResponssiveLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
